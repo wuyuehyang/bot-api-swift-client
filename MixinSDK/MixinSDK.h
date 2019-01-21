f4bc408c832c05395c488ef0bad88fb23d6b3ddd
wuyuehyang <wuyuehyang@gmail.com>
2019-01-21 19:30:26 +0800

    Initialize project

---
+++ MixinSDK/MixinSDK.h
@@ -0,0 +1,19 @@
+//
+//  MixinSDK.h
+//  MixinSDK
+//
+//  Created by wuyuehyang on 2019/1/21.
+//  Copyright © 2019 wuyuehyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MixinSDK.
+FOUNDATION_EXPORT double MixinSDKVersionNumber;
+
+//! Project version string for MixinSDK.
+FOUNDATION_EXPORT const unsigned char MixinSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MixinSDK/PublicHeader.h>
+
+
