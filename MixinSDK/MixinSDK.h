763e637a90d55658af6424f5f177c415be8cd330
wuyuehyang <wuyuehyang@gmail.com>
2019-01-22 16:10:31 +0800

    Support compilation for macOS

--- MixinSDK/MixinSDK.h
+++ MixinSDK/MixinSDK.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 wuyuehyang. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MixinSDK.
 FOUNDATION_EXPORT double MixinSDKVersionNumber;
