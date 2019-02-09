0dc2c3409d9515ba51c12fd6c7acae1851c92e17
wuyuehyang <wuyuehyang@gmail.com>
2019-02-10 02:44:14 +0800

    Refactor Data utilities

--- MixinSDK/Crypto/Data+Utils.swift
+++ MixinSDK/Crypto/Data+Utils.swift
@@ -1,5 +1,5 @@
 //
-//  Data+Base64.swift
+//  Data+Utils.swift
 //  MixinSDK
 //
 //  Created by wuyuehyang on 2019/1/23.
@@ -9,6 +9,17 @@
 import Foundation
 
 extension Data {
+    
+    init?(withSecuredRandomBytesOfCount count: Int) {
+        guard let bytes = malloc(count) else {
+            return nil
+        }
+        let status = SecRandomCopyBytes(kSecRandomDefault, count, bytes)
+        guard status == errSecSuccess else {
+            return nil
+        }
+        self.init(bytesNoCopy: bytes, count: count, deallocator: .free)
+    }
     
     func base64URLEncodedString() -> String {
         var str = base64EncodedString()
